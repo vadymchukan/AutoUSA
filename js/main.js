d0580e5b6122f83cd23992a3027c8646b66c4ee8
vadymchukan <89728628+vadymchukan@users.noreply.github.com>
2022-05-28 10:51:28 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $(".carusel__inner").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
